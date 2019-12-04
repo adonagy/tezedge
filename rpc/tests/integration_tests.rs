8301b4b62458b3fedf9fa22bef8159cf06cdf1e1
Adrian Nagy <adrian.nagy@simplestaking.com>
2019-12-04 17:06:24 +0000

    rework to get head from tezedge and concrete block from ocaml

--- rpc/tests/integration_tests.rs
+++ rpc/tests/integration_tests.rs
@@ -19,32 +19,42 @@ fn test_heads() {
         Ok(v) => v,
         Err(e) => panic!("Invalid json: {}", e),
     };
-    let ocaml_head = match get_head(NodeType::Ocaml) {
+
+    let block_id: String = rust_head["hash"].to_string();
+
+    let ocaml_block = match get_block(&block_id) {
         Ok(v) => v,
         Err(e) => panic!("Invalid json: {}", e),
     };
 
     // TODO: make it more obvious in the output
     //              actual      expected
-    assert_json_eq!(rust_head, ocaml_head);
+    assert_json_eq!(rust_head, ocaml_block);
+}
+
+fn get_block(block_id: &String) -> Result<serde_json::value::Value> {
+    let url = format!(
+        "{}{}",
+        "http://46.101.160.245:3000/chains/main/blocks/",
+        block_id.replace("\"", "")
+    );
+
+    let mut res = reqwest::blocking::get(&url)?;
+    let mut body = String::new();
+    res.read_to_string(&mut body)?;
+
+    Ok(serde_json::from_str(&body)?)
 }
 
 fn get_head(node_type: NodeType) -> Result<serde_json::value::Value> {
     let url = match node_type {
         NodeType::Ocaml => "http://46.101.160.245:3000/chains/main/blocks/head", // reference Ocaml node
-        NodeType::Tezedge => "http://localhost:18732/chains/main/blocks/head", // locally built Tezedge node
+        NodeType::Tezedge => "http://node-ru:18732/chains/main/blocks/head", // locally built Tezedge node
     };
-
-    // let url = format!("http://http://46.101.160.245:{}/chains/main/blocks/head", port);
 
     let mut res = reqwest::blocking::get(url)?;
     let mut body = String::new();
     res.read_to_string(&mut body)?;
-
-    // let json_string = match str::from_utf8() {
-    //     Ok(v) => v,
-    //     Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
-    // };
 
     Ok(serde_json::from_str(&body)?)
 }
